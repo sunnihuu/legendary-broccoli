f2e817f558131a7e0b713ee1d1d19dc37b1618ae
sunni0406 <jh5091@columbia.edu>
2025-06-05 13:26:16 -0400

    Events and UIs
    Interactive Web submission

---  weather-widget/javascript/main.js
+++  weather-widget/javascript/main.js
@@ -1,10 +1,22 @@
-console.log("this works");
+let input = document.querySelector(".zipcode");
+let btn = document.querySelector(".search-button");
 
-const API_KEY = config.WEATHER_API_KEY;
+getWeatherData = (zip) => {
+  let API_KEY = config.WEATHER_API_KEY;
+  let API_ENDPOINT = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&APPID=${API_KEY}`;
 
-    function getWeatherData() {
-  fetch(`https://api.openweathermap.org/data/2.5/weather?zip=10128&APPID=${API_KEY}`)
-  .then(response => response.json())
-  .then(data => console.log(data));
-}
-getWeatherData()
+  fetch(API_ENDPOINT)
+    .then((response) => response.json())
+    .then((data) => {
+      let local_weather_data = data;
+        console.log(local_weather_data)
+    })
+  }
+
+const getZipCode = (e) => {
+  e.preventDefault();
+  let ZIP_CODE = input.value;
+  getWeatherData(ZIP_CODE);
+};
+
+btn.addEventListener("click", getZipCode)
