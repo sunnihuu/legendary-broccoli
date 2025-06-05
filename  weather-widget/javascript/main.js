ea4e7f03ad69c71b18b9f41326ca7147796b28da
sunni0406 <jh5091@columbia.edu>
2025-06-05 11:29:05 -0400

    Data and APIs
    Interactive Web submission

---  weather-widget/javascript/main.js
+++  weather-widget/javascript/main.js
@@ -1,11 +1,10 @@
 console.log("this works");
+
 const API_KEY = config.WEATHER_API_KEY;
-fetch(`http://api.openweathermap.org/data/2.5/weather?zip=10128&APPID=${WEATHER_API_KEY}`)
-    .then(response => response.json())
-    .then(data => console.log(data));
 
     function getWeatherData() {
-  // fetch request goes in here :-)
+  fetch(`https://api.openweathermap.org/data/2.5/weather?zip=10128&APPID=${API_KEY}`)
+  .then(response => response.json())
+  .then(data => console.log(data));
 }
-
 getWeatherData()
