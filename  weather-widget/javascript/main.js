192bb92cd1374aa9a6306260f7af806f7f63833c
sunni0406 <jh5091@columbia.edu>
2025-06-05 15:14:18 -0400

    DOM Manipulation
    Interactive Web submission

---  weather-widget/javascript/main.js
+++  weather-widget/javascript/main.js
@@ -1,5 +1,10 @@
 let input = document.querySelector(".zipcode");
 let btn = document.querySelector(".search-button");
+let form = document.querySelector("form");
+let image = document.querySelector("img");
+
+let CITY_NAME = document.querySelector(".city_name");
+let CITY_TEMP = document.querySelector(".temperature");
 
 getWeatherData = (zip) => {
   let API_KEY = config.WEATHER_API_KEY;
@@ -7,10 +12,17 @@ getWeatherData = (zip) => {
 
   fetch(API_ENDPOINT)
     .then((response) => response.json())
-    .then((data) => {
+    .then(data => {
       let local_weather_data = data;
-        console.log(local_weather_data)
-    })
+      let WEATHER_ICON = local_weather_data.weather[0].icon;
+          image.setAttribute('src', 'http://openweathermap.org/img/wn/03d@2x.png');
+
+      CITY_NAME.textContent = local_weather_data.name;
+      let weather_in_celsius = Math.round(
+      local_weather_data.main.temp - 273
+      );
+      CITY_TEMP.textContent = weather_in_celsius + " C"
+  });
   }
 
 const getZipCode = (e) => {
@@ -20,3 +32,5 @@ const getZipCode = (e) => {
 };
 
 btn.addEventListener("click", getZipCode)
+form.reset();  
+input.focus(); 
